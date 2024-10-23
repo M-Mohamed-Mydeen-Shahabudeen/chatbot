315aa27bd8a4dfb9fb1e583f2fa34d37409ac949
Mohamed Mydeen Shahabudeen M <156303564+M-Mohamed-Mydeen-Shahabudeen@users.noreply.github.com>
2024-10-23 11:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-st.write("Welcome")
+st.write("Mohamed mydeen")
 genai.configure(api_key="AIzaSyCBwiYm-68EHhtEYBs7HmhNAG7L8HPKABE")
 text = text = st.text_input("Enter your question :")
 model = genai.GenerativeModel('gemini-pro')
